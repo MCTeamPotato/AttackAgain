c8567d64c2d1348655a4a87b9e692c3d1ff8b19f
KL_jiana <2436262516@qq.com>
2024-07-27 02:29:10 +0800

    1.18!

--- src/main/java/com/teampotato/attackagain/event/ForgeEvent.java
+++ src/main/java/com/teampotato/attackagain/event/ForgeEvent.java
@@ -4,10 +4,8 @@ import com.teampotato.attackagain.attribute.AttackAttribute;
 import com.teampotato.attackagain.config.AttackConfig;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,8 +18,8 @@ import static com.teampotato.attackagain.AttackAgain.MODID;
 public final class ForgeEvent {
     @SubscribeEvent
     public static void changeAttribute(PlayerEvent.PlayerLoggedInEvent event) {
-        Player player = event.getEntity();
-        if (!player.getPersistentData().contains("hasJoinedBefore")) {
+        Entity entity = event.getEntity();
+        if (entity instanceof Player player && !player.getPersistentData().contains("hasJoinedBefore")) {
             player.getAttribute(AttackAttribute.ATTACK_PROBABILITY.get()).setBaseValue(AttackConfig.PROBABILITY.get());
             player.getAttribute(AttackAttribute.ATTACK_DAMAGE.get()).setBaseValue(AttackConfig.DAMAGE.get());
             player.getAttribute(AttackAttribute.ATTACK_FREQUENCY.get()).setBaseValue(AttackConfig.FREQUENCY.get());
@@ -31,7 +29,7 @@ public final class ForgeEvent {
 
     @SubscribeEvent
     public static void attackAgain(LivingHurtEvent event) {
-        LivingEntity monster = event.getEntity();
+        Entity monster = event.getEntity();
         if (event.getSource().getEntity() instanceof ServerPlayer player && monster.isAlive()) {
             Random random = new Random();
             if (random.nextInt(100) + 1 <= player.getAttribute(AttackAttribute.ATTACK_PROBABILITY.get()).getValue()) {
